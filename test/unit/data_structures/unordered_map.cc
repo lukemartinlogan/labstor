b0ac4bcc8d981df597c2294e5d82c1ff0167ce09
lukemartinlogan <lukemartinlogan@gmail.com>
2022-12-16 21:20:13 -0600

    Add full single-thread map test back

--- test/unit/data_structures/unordered_map.cc
+++ test/unit/data_structures/unordered_map.cc
@@ -265,12 +265,11 @@ void UnorderedMapOfStringIntTest() {
     for (int i = 0; i < 100; ++i) {
       string i_text(std::to_string(i));
       map.emplace(i_text, i);
-      REQUIRE(map.find(i_text) != map.end());
     }
-    /*for (int i = 0; i < 100; ++i) {
+    for (int i = 0; i < 100; ++i) {
       string i_text(std::to_string(i));
       REQUIRE(map.find(i_text) != map.end());
-    }*/
+    }
   }
 }
 
