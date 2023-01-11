94f172405fb1a8f7bf61a37b43ce5dd61091f579
lukemartinlogan <lukemartinlogan@gmail.com>
2023-01-11 12:46:45 -0600

    Add -pg option to profile benchmarks

--- benchmark/data_structure/string.cc
+++ benchmark/data_structure/string.cc
@@ -9,11 +9,16 @@
 #include <labstor/data_structures/string.h>
 
 template<typename T>
-class StringTest {
+class StringTestSuite {
  public:
   std::string str_type_;
 
-  StringTest() {
+  /////////////////
+  /// Test Cases
+  /////////////////
+
+  /** Constructor */
+  StringTestSuite() {
     if constexpr(std::is_same_v<std::string, T>) {
       str_type_ = "std::string";
     } else if constexpr(std::is_same_v<lipc::string, T>) {
@@ -21,13 +26,7 @@ class StringTest {
     }
   }
 
-  void TestOutput(const std::string &test_name, Timer &t) {
-    printf("%s, %s, %lf\n",
-           test_name.c_str(),
-           str_type_.c_str(),
-           t.GetMsec());
-  }
-
+  /** Construct + destruct in a loop */
   void ConstructDestructTest(int count, int length) {
     char *data = (char *) malloc(length + 1);
     data[length] = 0;
@@ -41,11 +40,47 @@ class StringTest {
 
     TestOutput("ConstructDestructTest", t);
   }
+
+  /** Construct in a loop, and then destruct in a loop */
+  void ConstructThenDestructTest(int count, int length) {
+    char *data = (char *) malloc(length + 1);
+    data[length] = 0;
+
+    std::vector<T> vec(count);
+
+    Timer t;
+    t.Resume();
+    for (int i = 0; i < count; ++i) {
+      vec[i] = T(data);
+    }
+    vec.clear();
+    t.Pause();
+
+    TestOutput("ConstructThenDestructTest", t);
+  }
+
+  /////////////////
+  /// Test Output
+  /////////////////
+
+  /** Output test results */
+  void TestOutput(const std::string &test_name, Timer &t) {
+    printf("%s, %s, %lf\n",
+           test_name.c_str(),
+           str_type_.c_str(),
+           t.GetMsec());
+  }
 };
 
+template<typename T>
+void StringTest() {
+  StringTestSuite<T>().ConstructDestructTest(1000000, 10);
+  StringTestSuite<T>().ConstructThenDestructTest(1000000, 10);
+}
+
 void FullStringTest() {
-  StringTest<std::string>().ConstructDestructTest(1000000, 10);
-  StringTest<lipc::string>().ConstructDestructTest(1000000, 10);
+  StringTest<std::string>();
+  StringTest<lipc::string>();
 }
 
 TEST_CASE("StringBenchmark") {
