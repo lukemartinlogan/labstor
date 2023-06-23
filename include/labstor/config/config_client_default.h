cc31f4a3806eb97d0cee278b514c0949bd67ed27
lukemartinlogan <lukemartinlogan@gmail.com>
2023-06-23 10:51:44 -0500

    Commit config defaults

---
+++ include/labstor/config/config_client_default.h
@@ -0,0 +1,5 @@
+#ifndef LABSTOR_SRC_CONFIG_CLIENT_DEFAULT_H_
+#define LABSTOR_SRC_CONFIG_CLIENT_DEFAULT_H_
+const char* kClientDefaultConfigStr = 
+"thread_model: kStd\n";
+#endif  // LABSTOR_SRC_CONFIG_CLIENT_DEFAULT_H_
