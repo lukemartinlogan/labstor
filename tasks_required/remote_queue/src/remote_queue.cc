deca10d5749911a0c9debc7068fd4262920e0966
lukemartinlogan <lukemartinlogan@gmail.com>
2023-09-05 01:31:19 -0500

    Remove size?

--- tasks_required/remote_queue/src/remote_queue.cc
+++ tasks_required/remote_queue/src/remote_queue.cc
@@ -322,14 +322,13 @@ class Server : public TaskLib {
       auto *queue = LABSTOR_QM_CLIENT->GetQueue(QueueId(state_id));
       queue->Emplace(orig_task->lane_hash_, p);
       bool is_fire_forget = orig_task->IsFireAndForget();
-      HILOG(kDebug, "(node {}) Executing task (task_node={}, task_state={}/{}, method={}, f&f={}, size={})",
+      HILOG(kDebug, "(node {}) Executing task (task_node={}, task_state={}/{}, method={}, f&f={})",
             LABSTOR_QM_CLIENT->node_id_,
             orig_task->task_node_,
             orig_task->task_state_,
             state_id,
             method,
-            is_fire_forget,
-            xfer[0].data_size_);
+            is_fire_forget);
 
       // Get return value (should not contain data)
       if (!is_fire_forget) {
